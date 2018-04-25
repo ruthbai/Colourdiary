c92e3fa33e13a51c8d09155d98107abc83f67afe
Ruthbai <minjoobai@gmail.com>
2018-04-25 22:24:46 +1200

    postgres heroku?

--- knexfile.js
+++ knexfile.js
@@ -49,7 +49,14 @@ module.exports = {
     },
     migrations: {
       tableName: 'knex_migrations'
-    }
+    },
+    production: {
+      client: 'postgresql',
+      connection: {
+        database: 'postgres://fsqdkccnqvlfdw:44928630c14f82a589a986c79dca36af72214445e500fd8bb1a60f13ea280b1f@ec2-50-16-196-238.compute-1.amazonaws.com:5432/ddthdujnfcsvli',
+        user:     'username',
+        password: 'password'
+      }
   }
-
+  }
 };
